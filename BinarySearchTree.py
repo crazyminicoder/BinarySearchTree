b62358f1da558ab4ad061d9c85d08c32a359a304
Syed Wali Uddin Quadri <waliwasser29@gmail.com>
2024-08-22 14:52:15 -0500

    Updated the BST by adding more methods: findMin, findMax, findHeight and search

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -46,6 +46,60 @@ class BinarySearchTree:
             self.postOrder(root.right)
             print(root.data, end=' ')
 
+    def findMin(self, root):
+        if root is None:
+            print('Tree is empty')
+            return
+        temp = root
+        while temp.left is not None:
+            temp = temp.left
+
+        print('The minimum value in the tree is:', temp.data)
+
+    def findMax(self, root):
+        if root is None:
+            print('Tree is empty')
+            return
+        temp = root
+        while temp.right is not None:
+            temp = temp.right
+
+        print('The maximum value in tree is:', temp.data)
+
+    def findHeight(self, root):
+        countLeft = 0
+        countRight = 0
+
+        temp1 = root
+        temp2 = root
+        while temp1.left is not None:
+            temp1 = temp1.left
+            countLeft += 1
+
+        while temp2.right is not None:
+            temp2 = temp2.right
+            countRight += 1
+
+        print("The height of the tree is:", max(countLeft, countRight))
+
+    def search(self, root, key):
+        if self.root is None:
+            print("The tree is empty")
+        else:
+            self._search(root, key)
+
+    def _search(self, root, key):
+        if root is None:
+            print("Key not found")
+            return
+        if root.data == key:
+            print("Key found")
+            return
+        elif key < root.data:
+            self._search(root.left, key)
+        else:
+            self._search(root.right, key)
+
 
 bt = BinarySearchTree()
 
@@ -68,3 +122,11 @@ bt.preOrder(bt.root)
 print("\n")
 print('PostOrder:', end="\n")
 bt.postOrder(bt.root)
+
+print("\n")
+bt.findMin(bt.root)
+bt.findMax(bt.root)
+
+bt.findHeight(bt.root)
+
+bt.search(bt.root, 2)
